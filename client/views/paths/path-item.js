989394c1e94c6db80be1fd7c48b8fe30d22b3e99
Andy Wang <AndrewWang996@gmail.com>
2015-11-07 20:08:09 -0500

    started to implement "edit path" functionality

---
+++ client/views/paths/path-item.js
@@ -0,0 +1,19 @@
+Template.pathItemHeader.events({
+
+    /**
+        Remove path from the Paths Meteor Collection
+        Also reactively removes path from the HTML
+    */
+    'click .js-delete-path': function(event, template) {
+        Meteor.call('removePath', this._id, function (error) {
+            if (error)
+                alert(error.reason)
+        });
+    },
+    'click .js-edit-path': function(event, template) {
+    	Router.route("/paths/:fileName");
+    	console.log("hi");
+    }
+});
+
+Template.pathItem
--- client/views/paths/paths.js
+++ client/views/paths/paths.js
@@ -9,20 +9,6 @@ Template.pathsHeader.events({
     }
 });
 
-Template.pathItemHeader.events({
-
-    /**
-        Remove path from the Paths Meteor Collection
-        Also reactively removes path from the HTML
-    */
-    'click .js-delete-path': function(event, template) {
-        Meteor.call('removePath', this._id, function (error) {
-            if (error)
-                alert(error.reason)
-        });
-    }
-});
-
 Template.paths.helpers({
     /**
         Return all paths in the Paths Meteor Collection, newest first.
